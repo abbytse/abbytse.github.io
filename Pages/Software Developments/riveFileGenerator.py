948ce673361ba430a36a7e18d6a4243176080281
xabbirix <atse5@fordham.edu>
2020-03-05 14:10:28 -0500

    rivescript File Generator

---
+++ Pages/Software Developments/riveFileGenerator.py
@@ -0,0 +1,16 @@
+import csv 
+import os
+
+here = os.path.dirname(os.path.abspath(__file__))
+filename = os.path.join(here, 'paintStore.csv')
+
+with open(filename, mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        print(f'\t{row["name"]} works in the {row["department"]} department, and was born in {row["birthday month"]}.')
+        line_count += 1
+    print(f'Processed {line_count} lines.')
